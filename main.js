66f756cb99a6655fd07580628379768380eb3bd8
aadhyansh <73935325+aadhyansh@users.noreply.github.com>
2021-02-25 12:27:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition=new SpeechRecognition();
+function start() {
+document.getElementById("textbox").innerHTML="";
+recognition.start();
+}
+recognition.onresult=function(event) {
+console.log(event);
+var Content=event.results[0][0].transcript;
+document.getElementById("textbox").innerHTML=Content;
+console.log(Content);
+if(Content=="take my selfie") {
+console.log("Taking Selfie");
+speak();
+}
+}
+function speak() {
+var synth=window.speechSynthesis;
+speak_data=document.getElementById("textbox").value;
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+Webcam.attach( camera );
+}
+Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+  });
+  camera=document.getElementById("camera");
+ 
